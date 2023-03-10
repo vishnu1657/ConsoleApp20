b3c30a9999a07102f011aff346c350d52e6bf72c
HP <HP@DESKTOP-4Q8V1JF>
2023-03-10 15:42:18 +0530

    Add project files.

---
+++ ConsoleApp20/Arithmetic.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp20
+{
+    internal class Arithmetic
+    {
+
+        public int A { get; set; }
+        public int B { get; set; }
+
+        //simple  parameters or arguments 
+        public void Add(int num1, int num2)
+        {
+            int result = num1 + num2;
+
+            //Console.WriteLine("Addition of " + num1 + "and" + num2 + "is = " + result);
+            Console.WriteLine($"Addition of {num1} and {num2} is = {result}");
+
+            //Addition of 10 and 20 is = 30
+
+        }
+
+        // function complex aruments or parameters 
+        public void sub(Arithmetic arth)
+        {
+            int result = arth.A + arth.B;
+
+            Console.WriteLine($"Substraction of {arth.A} and {arth.B} is = {result}");
+        }
+
+        public void mul(int num1, int num2, out int result)   // output parameter
+        {
+            result = num1 * num2;
+
+            
+        }
+
+        public void Div(int num1, int num2, ref int result1)   // input - output parameter
+        {
+            result1 = result1 + num1 / num2;
+
+          
+
+
+        }
+    }    
+}
+    
+
---
+++ ConsoleApp20/Program.cs
@@ -0,0 +1,27 @@
+﻿using ConsoleApp20;
+
+
+public class program
+{
+    public static void Main()
+    {
+        Arithmetic arithmetic1 = new Arithmetic();
+        arithmetic1.A = 20;
+        arithmetic1.B = 10;
+
+       Arithmetic arithmetic2 = new Arithmetic();
+        arithmetic2.sub(arithmetic1);
+
+
+
+        //arithmetic.Add (10,20); 
+
+        int result;
+        arithmetic2.mul(10, 20, out result);
+        Console.WriteLine($"Multiplication of 10 and  30 is = {result}");
+
+        int result1 = 10;
+        arithmetic2.Div(20,10,ref result1);
+        Console.WriteLine($"Result = {result1}");
+    }
+}
